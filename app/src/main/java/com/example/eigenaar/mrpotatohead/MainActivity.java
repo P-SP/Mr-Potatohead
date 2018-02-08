957d2fd50da20d453a8f5b4cd7790ce16611ae79
P-SP <31902611+P-SP@users.noreply.github.com>
2018-02-08 20:13:01 +0100

    Update: everything but 'Memory management' works

--- app/src/main/java/com/example/eigenaar/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/eigenaar/mrpotatohead/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.eigenaar.mrpotatohead;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // To store all the name and id's of the images
+    String[] images_name ={"eyes", "eyebrows", "glasses", "mustache", "ears", "nose", "mouth", "arms", "hat", "shoes"};
+    ImageView[] images_id;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Store all id's only one time (in the same order!)
+        images_id = new ImageView[]{
+                findViewById(R.id.eyes),
+                findViewById(R.id.eyebrows),
+                findViewById(R.id.glasses),
+                findViewById(R.id.mustache),
+                findViewById(R.id.ears),
+                findViewById(R.id.nose),
+                findViewById(R.id.mouth),
+                findViewById(R.id.arms),
+                findViewById(R.id.hat),
+                findViewById(R.id.shoes)};
+    }
+
+    public void checkClicked(View v) {
+        CheckBox checkbox = (CheckBox) v;
+        String clicked_item = checkbox.getText().toString();
+
+        for (int i = 0; i < images_name.length; i++){
+            if (images_name[i].equals(clicked_item)){
+                if (checkbox.isChecked()) {
+                    images_id[i].setVisibility(View.VISIBLE);
+                } else {
+                    images_id[i].setVisibility(View.INVISIBLE);
+                }
+            }
+        }
+
     }
 }
